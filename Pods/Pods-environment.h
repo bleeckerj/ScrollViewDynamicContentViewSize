d0787be13215aff36c32a10150839b06c50a55c3
Julian Bleecker <julian@nearfuturelaboratory.com>
2014-07-07 06:14:59 -0700

    initial project version

---
+++ Pods/Pods-environment.h
@@ -0,0 +1,56 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// FPBrandColors
+#define COCOAPODS_POD_AVAILABLE_FPBrandColors
+#define COCOAPODS_VERSION_MAJOR_FPBrandColors 0
+#define COCOAPODS_VERSION_MINOR_FPBrandColors 1
+#define COCOAPODS_VERSION_PATCH_FPBrandColors 3
+
+// FXLabel
+#define COCOAPODS_POD_AVAILABLE_FXLabel
+#define COCOAPODS_VERSION_MAJOR_FXLabel 1
+#define COCOAPODS_VERSION_MINOR_FXLabel 5
+#define COCOAPODS_VERSION_PATCH_FXLabel 8
+
+// Masonry
+#define COCOAPODS_POD_AVAILABLE_Masonry
+#define COCOAPODS_VERSION_MAJOR_Masonry 0
+#define COCOAPODS_VERSION_MINOR_Masonry 5
+#define COCOAPODS_VERSION_PATCH_Masonry 2
+
+// NSLogger
+#define COCOAPODS_POD_AVAILABLE_NSLogger
+#define COCOAPODS_VERSION_MAJOR_NSLogger 1
+#define COCOAPODS_VERSION_MINOR_NSLogger 2
+#define COCOAPODS_VERSION_PATCH_NSLogger 0
+
+// ObjectiveSugar
+#define COCOAPODS_POD_AVAILABLE_ObjectiveSugar
+#define COCOAPODS_VERSION_MAJOR_ObjectiveSugar 1
+#define COCOAPODS_VERSION_MINOR_ObjectiveSugar 1
+#define COCOAPODS_VERSION_PATCH_ObjectiveSugar 0
+
+// Reveal-iOS-SDK
+#define COCOAPODS_POD_AVAILABLE_Reveal_iOS_SDK
+#define COCOAPODS_VERSION_MAJOR_Reveal_iOS_SDK 1
+#define COCOAPODS_VERSION_MINOR_Reveal_iOS_SDK 0
+#define COCOAPODS_VERSION_PATCH_Reveal_iOS_SDK 4
+
+// TTTAttributedLabel
+#define COCOAPODS_POD_AVAILABLE_TTTAttributedLabel
+#define COCOAPODS_VERSION_MAJOR_TTTAttributedLabel 1
+#define COCOAPODS_VERSION_MINOR_TTTAttributedLabel 9
+#define COCOAPODS_VERSION_PATCH_TTTAttributedLabel 4
+
+// iCarousel
+#define COCOAPODS_POD_AVAILABLE_iCarousel
+#define COCOAPODS_VERSION_MAJOR_iCarousel 1
+#define COCOAPODS_VERSION_MINOR_iCarousel 7
+#define COCOAPODS_VERSION_PATCH_iCarousel 6
+
